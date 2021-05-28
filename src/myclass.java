93a3c5e0e0ab59886ae192b6f68b17c35b96178c
”Alex <person12@posteo.net>
2021-05-28 12:14:53 +0400

    Initial commit

---
+++ src/myclass.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class myclass {
+    public static void main(String[] args){
+    JFrame frame = new JFrame("notepad");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}};
